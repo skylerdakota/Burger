6af8ea80d516edc98d6b42cec635e2cff55b8d91
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-11-24 12:33:48 -0500

    added models folder, burger.js file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", "false");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", "false");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Fried Chicken Sanwich", "false");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Fried Chicken Sanwich");
