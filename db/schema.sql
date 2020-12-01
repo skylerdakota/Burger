be6a87f7959ac7c7e734bc0bc7e094685751eefc
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-12-01 18:25:28 -0500

    updating jawsDB connection

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT, 
   burger_name VARCHAR(50) NOT NULL,
   devoured BOOLEAN DEFAULT false,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
 
